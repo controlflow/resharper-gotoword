ac48a522a21853669a210373ba64d63c17478e21
ControlFlow <shvedov89@gmail.com>
2013-07-15 20:04:40 +0400

    Readme

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,19 +7,15 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Shvedov Alexander")]
 [assembly: AssemblyProduct("ReSharper.GoToWord")]
-[assembly: AssemblyCopyright("Copyright © Sjvedov Alexander, 2013")]
+[assembly: AssemblyCopyright("Copyright © Shvedov Alexander, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-#if RESHARPER7
-[assembly: ActionsXml("ReSharper.GoToWord.R7.Actions.xml")]
-#elif RESHARPER8
-[assembly: ActionsXml("ReSharper.GoToWord.R8.Actions.xml")]
-#endif
+[assembly: ActionsXml("ReSharper.GoToWord.Actions.xml")]
 
 [assembly: PluginTitle("Go to word")]
-[assembly: PluginDescription("Words navigation plugin for ReSharper")]
+[assembly: PluginDescription("Textual navigation plugin for ReSharper")]
 [assembly: PluginVendor("Shvedov Alexander")]
--- README.md
+++ README.md
@@ -4,14 +4,24 @@ ReSharper Go to Word plugin
 This plugin allows you to utilize the power of R# caches and occurances browsing
 UI capabilities (with various groupings and previews) for simple textual search.
 
+#### Features
 
 
-### Known issues
+
+#### Installation
+
+This plugin is available in ReSharper 8 Extension Gallery:
+
+pic
+
+#### Known issues
 
 * By now, ReSharper plugins are not able to register hotkeys after installation
 (because of VS integration issues). You can assign keyboard shortcut manually
 by using VisualStudio's *Options* - *Environment* - *Keyboard* dialog,
 just find *"ReSharper_GotoWordIndex"* action.
+
+![hotkeys](/Content/hotkeys.png)
 
 * This plugin highly relays on infrastructure changes introduced in ReSharper 8.0
 (universal word index cache for language-indendent tokenization), so it will not
